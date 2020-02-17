57aacffd3af81596a42fd65837277da9e6673fa0
Arpith Siromoney <arpith@feedreader.co>
2020-02-17 15:12:05 -0800

    Updates

---
+++ precache-manifest.607c227fdb1d285819f1cb6f7eee9284.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6db8902d9219f7d6d65e4c20a60fd35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c972b1dec740f85440ba",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "996397ca12a76c4ee8ac",
+    "url": "/static/js/2.09e00f66.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.09e00f66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c972b1dec740f85440ba",
+    "url": "/static/js/main.e79523dc.chunk.js"
+  },
+  {
+    "revision": "bc698569b839cb39bd54",
+    "url": "/static/js/runtime-main.e806c15e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
